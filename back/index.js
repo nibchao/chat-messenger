896d40548d7faf8e0a707b0fc08dda1035a3af4c
Nicholas Chao <nibchao@gmail.com>
2023-06-03 13:31:45 -0700

    added nodemon and npm start script

--- back/index.js
+++ back/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const socketIO = require('socket.io');
 const http = require('http');
@@ -11,9 +10,7 @@ const bodyParser = require( 'body-parser');
 const app = express(); 
 const server = http.createServer(app);
 
-
-
-// TODO: add cors to allow cross origin requests
+// cors to allow cross origin requests
 const io = socketIO(server, {
   cors: {
     origin: '*',
@@ -21,25 +18,18 @@ const io = socketIO(server, {
 });
 app.use(cors({origin: 'http://localhost:3000', credentials:true }))
 
-
-
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
 // Connect to the database
-// TODO: your code here
 mongoose.connect(process.env.MONGO_URL);
 const database = mongoose.connection;
 
 database.on('error', (error) => console.error(error));
 database.once('open', () => console.log('Connected to Database'));
 
-
 // Set up the session
-// TODO: your code here
 const sessionMiddleware = session({
   resave: false, // Whether to save the session to the store on every request
   saveUninitialized: false, // Whether to save uninitialized sessions to the store
@@ -48,11 +38,8 @@ const sessionMiddleware = session({
 
 app.use(sessionMiddleware);
 
-
-
 const routes = require('./routes/auth');
 const rooms = require('./routes/rooms');
-
 
 app.get('/', (req, res) => {
   if (req.session && req.session.authenticated) {

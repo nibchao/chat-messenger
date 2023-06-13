1c2ec331e71d281ba363c92a83dc25ec5da39ad5
Nicholas Chao <nibchao@gmail.com>
2023-06-12 20:42:55 -0700

    added sender username and timestamp to chat

--- back/routes/rooms.js
+++ back/routes/rooms.js
@@ -112,3 +112,15 @@ router.post("/messages", async (req, res) => {
     }
   }
 });
+
+router.post('/getUsernames', async (req, res) => {
+  let userIDArray = req.body.userIDArray;
+  let usernameArray = [];
+
+  for (let cnt = 0; cnt < userIDArray.length; cnt++)
+  {
+    let user = await User.findById({ "_id": userIDArray[cnt] });
+    usernameArray[cnt] = user.username;
+  }
+  res.status(200).json(usernameArray);
+})
